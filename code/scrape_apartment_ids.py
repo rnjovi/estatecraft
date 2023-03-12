121950d79060b2a8db3ef3ff714c9b4d99b97e44
rnjovi <joona.vihervaara@tuni.fi>
2023-03-12 15:27:17 +0200

    Added support for more than 6 pages

--- code/scrape_apartment_ids.py
+++ code/scrape_apartment_ids.py
@@ -4,28 +4,35 @@ from bs4 import BeautifulSoup
 def get_apartment_ids():
     base_url = 'https://www.etuovi.com/myytavat-asunnot'
     query_string = 'haku=M1954069489'
-    current_page = 1
+    current_page_num = 1
 
-    ids = []
-    all_ids = set()
+    unique_ids = set()
+    all_ids = []
 
-    while True:
-        url = f"{base_url}?{query_string}&sivu={current_page}"
-        response = requests.get(url)
+    # get the number of pages from the first page of the search results
+    first_page_url = f"{base_url}?{query_string}&sivu=1"
+    response = requests.get(first_page_url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+   
+    elements = soup.find_all('button', {'class': 'theme__button__1YqFK theme__flat__13aFK theme__button__1YqFK theme__squared__17Uvn theme__neutral__1F1Jf Button__button__3K-jn Pagination__button__3H2wX'})
+    last_element = elements[-1]
+    value = int(last_element.text.strip())
+
+    # loop through all pages
+    while current_page_num <= value:
+        current_page_url = f"{base_url}?{query_string}&sivu={current_page_num}"
+        response = requests.get(current_page_url)
         soup = BeautifulSoup(response.content, 'html.parser')
-        links = soup.find_all('div', {'class': 'ListPage__cardContainer__39dKQ'})
+        apartment_links = soup.find_all('div', {'class': 'ListPage__cardContainer__39dKQ'})
 
-        for link in links:
-            link2 = link.find('a', {'class': 'mui-style-58tli6 e12nd9f313'})
-            id = link2.get('id')
-            if id in all_ids:
+        for link in apartment_links:
+            apartment_link = link.find('a', {'class': 'mui-style-58tli6 e12nd9f313'})
+            apartment_id = apartment_link.get('id')
+            if apartment_id in all_ids:
                 continue
-            ids.append(id)
-            all_ids.add(id)
+            all_ids.append(apartment_id)
+            unique_ids.add(apartment_id)
 
-        if len(links) / current_page == 30:
-            current_page += 1
-        else:
-            break
+        current_page_num += 1
 
-    return ids
+    return all_ids
