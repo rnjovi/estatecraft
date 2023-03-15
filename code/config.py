195f238c4424e20197f650cdb65da2f496f5aa91
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 22:54:29 +0200

    Updated README

--- code/config.py
+++ code/config.py
@@ -1,24 +1,20 @@
+# Database configuration
 DB_NAME = "real_estate_info"
 DB_USER = "postgres"
 DB_PASSWORD = "new_password"
 
+# Application-specific configuration
+# Type keyword to filter apartments by type (e.g., "Kerrostalo")
 TYPE_KEYWORD = "Kerrostalo"
+
+# Location keyword to filter apartments by location (e.g., "Pirkkala")
 LOCATION_KEYWORD = "Pirkkala"
 
+# Search key for the specific search criteria used in the scraping process
 SEARCH_KEY = "M1955774866"
 
-"""
-For Hakuehdot: Suomi · Kerrostalo · Rivitalo · Omistusasunto
-M1955760892
-For Hakuehdot: Pirkkala · Kerrostalo · 4h · Omistusasunto
-M1955774866
-"""
-
-"""
-TODO:
-    online/offline column
-    talosivut ja niiden tiedot
-    omakotitalot pois
-    automaattinne päivittyminen
-    tietojen kerääminen tiedostoon / tietojen noutaminen aikaleimoittain.
-"""
+# Example search keys for different search criteria
+# For Hakuehdot: Suomi · Kerrostalo · Rivitalo · Omistusasunto
+# SEARCH_KEY = "M1955760892"
+# For Hakuehdot: Pirkkala · Kerrostalo · 4h · Omistusasunto
+# SEARCH_KEY = "M1955774866"
