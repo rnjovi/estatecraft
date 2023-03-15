f752112871f122825eb82c44a94c7c9dda9f6674
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 13:00:29 +0200

    getted rid of cProfile

--- code/main.py
+++ code/main.py
@@ -5,25 +5,20 @@ from scrape_apartment_ids import get_apartment_ids
 from table_creation import create_table, save_data, sql_query
 
 if __name__ == '__main__':
-    with cProfile.Profile() as profile:
-        # get the list of apartment IDs to scrape
-        ids = get_apartment_ids()
+    # get the list of apartment IDs to scrape
+    ids = get_apartment_ids()
 
-        # create a table to store the scraped data
-        create_table()
+    # create a table to store the scraped data
+    create_table()
 
-        # scrape data for each apartment ID
-        apartment_data = []
-        c = 1
-        for id in ids:
-            data = scrape_apartment_data(id)
-            apartment_data.append(data)
-            save_data(data)
-            print(c)
-            c = c + 1
+    # scrape data for each apartment ID
+    apartment_data = []
+    c = 1
+    for id in ids:
+        data = scrape_apartment_data(id)
+        apartment_data.append(data)
+        save_data(data)
+        print(c)
+        c = c + 1
 
-        # sql_query("SELECT * FROM apartments")
-    
-    results = pstats.Stats(profile)
-    results.sort_stats(pstats.SortKey.TIME)
-    results.print_stats()
+    # sql_query("SELECT * FROM apartments")
