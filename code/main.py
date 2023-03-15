de0ced4147afd9444d9e94d4064b52cba0bbd190
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 13:08:59 +0200

    added performance timers

--- code/main.py
+++ code/main.py
@@ -1,14 +1,22 @@
-import cProfile
-import pstats
+from timeit import default_timer as timer
 from scrape_apartment_data import scrape_apartment_data
 from scrape_apartment_ids import get_apartment_ids
 from table_creation import create_table, save_data, sql_query
 
 if __name__ == '__main__':
-    # get the list of apartment IDs to scrape
+    # measure the runtime of get_apartment_ids
+    start = timer()
     ids = get_apartment_ids()
+    end = timer()
+    runtime = end - start
+    print(f"get_apartment_ids runtime: {runtime} seconds")
 
-    # create a table to store the scraped data
+    # calculate the average time per ID
+    avg_time_per_id = runtime / len(ids)
+    print(f"Average time per ID: {avg_time_per_id} seconds")
+
+    """
+        # create a table to store the scraped data
     create_table()
 
     # scrape data for each apartment ID
@@ -20,5 +28,8 @@ if __name__ == '__main__':
         save_data(data)
         print(c)
         c = c + 1
+
+    """
+
 
     # sql_query("SELECT * FROM apartments")
--- code/scrape_apartment_ids.py
+++ code/scrape_apartment_ids.py
@@ -3,37 +3,42 @@ from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 from lxml import html
 
-def get_apartment_ids():
-    base_url = 'https://www.etuovi.com/myytavat-asunnot'
-    query_string = 'haku=M1955542998'
+def create_session():
+    return requests.Session()
 
-    # Create a session object for reusing the underlying TCP connection
-    session = requests.Session()
-
-    # Get the number of pages from the first page of the search results
+def get_total_pages(session, base_url, query_string):
     first_page_url = f"{base_url}?{query_string}&sivu=1"
     response = session.get(first_page_url)
     soup = BeautifulSoup(response.content, 'lxml')
 
     elements = soup.find_all('button', {'class': 'theme__button__1YqFK theme__flat__13aFK theme__button__1YqFK theme__squared__17Uvn theme__neutral__1F1Jf Button__button__3K-jn Pagination__button__3H2wX'})
     last_element = elements[-1] if elements else None
-    value = int(last_element.text.strip()) if last_element else 1
+    total_pages = int(last_element.text.strip()) if last_element else 1
 
+    return total_pages
+
+def fetch_and_parse_page(session, base_url, query_string, page_num):
+    current_page_url = f"{base_url}?{query_string}&sivu={page_num}"
+    response = session.get(current_page_url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    apartment_links = soup.find_all('div', {'class': 'ListPage__cardContainer__39dKQ'})
+
+    new_ids = [link.find('a', {'class': 'mui-style-58tli6 e12nd9f313'}).get('id') for link in apartment_links]
+    return new_ids
+
+def get_apartment_ids():
+    base_url = 'https://www.etuovi.com/myytavat-asunnot'
+    query_string = 'haku=M1955542998'
+
+    session = create_session()
+    total_pages = get_total_pages(session, base_url, query_string)
     unique_ids = []
 
-    def fetch_and_parse_page(page_num):
-        current_page_url = f"{base_url}?{query_string}&sivu={page_num}"
-        response = session.get(current_page_url)
-        soup = BeautifulSoup(response.content, 'lxml')
-        apartment_links = soup.find_all('div', {'class': 'ListPage__cardContainer__39dKQ'})
-
-        # Use list comprehension to extract apartment ids
-        new_ids = [link.find('a', {'class': 'mui-style-58tli6 e12nd9f313'}).get('id') for link in apartment_links]
-        return new_ids
-
-    # Use ThreadPoolExecutor to parallelize fetching and parsing webpages
     with ThreadPoolExecutor() as executor:
-        for new_ids in executor.map(fetch_and_parse_page, range(1, value+1)):
+        fetch_and_parse = lambda page_num: fetch_and_parse_page(session, base_url, query_string, page_num)
+        for new_ids in executor.map(fetch_and_parse, range(1, total_pages + 1)):
             unique_ids += new_ids
 
     return unique_ids
+
+print(get_apartment_ids())
