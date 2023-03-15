c55f78df2a4b676734f360baec9ea0bc6182e231
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 22:45:46 +0200

    changes to queries structure

--- code/app.py
+++ code/app.py
@@ -1,9 +1,15 @@
+from config import TYPE_KEYWORD, LOCATION_KEYWORD
 from utils.runtime_measures import measure_get_apartment_ids_runtime, measure_scrape_apartment_data_runtime
 from database.table_creation import create_apartments_table
 from database.queries import run_queries
 
 
 def main():
+    """
+    Main function to run the application, including measuring runtime,
+    creating the apartments table, scraping apartment data, and running queries.
+    """
+
     # measure the runtime of get_apartment_ids
     ids = measure_get_apartment_ids_runtime()
 
@@ -14,6 +20,8 @@ def main():
     measure_scrape_apartment_data_runtime(ids)
 
     # Run queries
-    run_queries()
+    run_queries(TYPE_KEYWORD, LOCATION_KEYWORD)
+
+
 if __name__ == '__main__':
     main()
