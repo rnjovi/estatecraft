e05eedf06b410b62d2f852fa96e096af88eabdc7
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 13:59:45 +0200

    Added printouts

--- code/main.py
+++ code/main.py
@@ -3,32 +3,37 @@ from scrape_apartment_data import scrape_apartment_data
 from scrape_apartment_ids import get_apartment_ids
 from table_creation import create_apartments_table, save_data, sql_query
 
-if __name__ == '__main__':
-    # measure the runtime of get_apartment_ids
+def measure_get_apartment_ids_runtime():
     start = timer()
     ids = get_apartment_ids()
     end = timer()
     runtime = end - start
     print(f"get_apartment_ids runtime: {runtime} seconds")
+    return ids
 
-    # calculate the average time per ID
-    avg_time_per_id = runtime / len(ids)
-    print(f"Average time per ID: {avg_time_per_id} seconds")
+def measure_scrape_apartment_data_runtime(ids):
+    total_scrape_time = 0
+    for id in ids:
+        start = timer()
+        data = scrape_apartment_data(id)
+        end = timer()
+        save_data(data)
+        total_scrape_time += (end - start)
+    avg_scrape_time = total_scrape_time / len(ids)
+    print(f"Scraped {len(ids)} apartments")
+    print(f"Average time per apartment scrape: {avg_scrape_time} seconds")
 
-        # create a table to store the scraped data
+def main():
+    # measure the runtime of get_apartment_ids
+    ids = measure_get_apartment_ids_runtime()
+
+    # create a table to store the scraped data
     create_apartments_table()
 
     # scrape data for each apartment ID
-    apartment_data = []
-    c = 1
-    for id in ids:
-        data = scrape_apartment_data(id)
-        apartment_data.append(data)
-        save_data(data)
-        print(c)
-        c = c + 1
-
-
-
+    measure_scrape_apartment_data_runtime(ids)
 
     # sql_query("SELECT * FROM apartments")
+
+if __name__ == '__main__':
+    main()
