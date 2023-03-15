69cc7e2263da395927c186a3af93190ce6715089
rnjovi <joona.vihervaara@tuni.fi>
2023-03-15 23:22:53 +0200

    Added docstrings

--- code/database/database.py
+++ code/database/database.py
@@ -1,9 +1,16 @@
-# database.py
-
 import psycopg2
 
 class Database:
     def __init__(self, dbname, user, password, host="localhost", port="5432"):
+        """
+        Initialize a connection to the database.
+
+        :param dbname: Database name
+        :param user: Username for the database
+        :param password: Password for the database
+        :param host: Host where the database is running (default: "localhost")
+        :param port: Port for the database connection (default: "5432")
+        """
         self.conn = psycopg2.connect(
             dbname=dbname,
             user=user,
@@ -13,6 +20,12 @@ class Database:
         )
     
     def execute(self, query, *args):
+        """
+        Execute a query on the database.
+
+        :param query: SQL query string
+        :param args: Optional query parameters
+        """
         try:
             cur = self.conn.cursor()
             cur.execute(query, *args)
@@ -22,6 +35,13 @@ class Database:
             print(f"Error: {e}")
 
     def fetch(self, query, *args):
+        """
+        Execute a query and fetch all results.
+
+        :param query: SQL query string
+        :param args: Optional query parameters
+        :return: List of results
+        """
         try:
             cur = self.conn.cursor()
             cur.execute(query, *args)
@@ -32,6 +52,13 @@ class Database:
             print(f"Error: {e}")
 
     def fetch_one(self, query, params=None):
+        """
+        Execute a query and fetch the first result.
+
+        :param query: SQL query string
+        :param params: Optional query parameters
+        :return: First result of the query
+        """
         with self.conn.cursor() as cur:
             cur.execute(query, params)
             result = cur.fetchone()
